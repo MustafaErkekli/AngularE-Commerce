3db9bf09b20b27e98f4c642a0d4b1417e02e170b
MustafaErkekli <82001577+MustafaErkekli@users.noreply.github.com>
2024-02-22 01:50:07 +0300

    Add project files.

---
+++ Data.API/DataContext/StoreContext.cs
@@ -0,0 +1,13 @@
+﻿using Data.API.DbModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.API.DataContext
+{
+	public class StoreContext : DbContext
+	{
+		public StoreContext(DbContextOptions options) : base(options)
+		{
+		}
+		public DbSet<Product>Products { get; set; }
+	}
+}
